c287b8bc8c6036c6c81d83293259693d0b89d7f4
j0e3gan <j0e3gan@0dfdc217-b534-4f0b-b730-bb753d44d08c>
2014-09-01 21:28:13 +0000

    DEV: Updated copyright notices to use the internationally recognized copyright symbol ©.

--- src/java/de/agentlab/svnstat/CLI.java
+++ src/java/de/agentlab/svnstat/CLI.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2006 Juergen Lind, jli@agentlab.de
+ * Copyright © 2006 Juergen Lind (jli@agentlab.de).
  * 
  * This program is free software; you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation; either version 2.1 of
